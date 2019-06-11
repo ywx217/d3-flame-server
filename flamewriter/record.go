4f6e5f6a748bcb40ae613a4eb9a2d6a89d64e94a
杨文轩 <gzyangwenxuan@corp.netease.com>
2019-06-11 14:01:03 +0800

    make members public

--- flamewriter/record.go
+++ flamewriter/record.go
@@ -2,27 +2,27 @@ package flamewriter
 
 // Record flame record with call stack info and statistical values
 type Record struct {
-	name     string
-	value    int
-	children map[string]*Record
+	Name     string
+	Value    int
+	Children map[string]*Record
 }
 
 // NewRecord creates a new Record
 func NewRecord(name string, value int) *Record {
 	return &Record{
-		name:     name,
-		value:    value,
-		children: make(map[string]*Record, 0),
+		Name:     name,
+		Value:    value,
+		Children: make(map[string]*Record, 0),
 	}
 }
 
 // Add adds a callstack
 func (r *Record) Add(stack []string, value int) {
-	r.value += value
+	r.Value += value
 	if len(stack) == 0 {
 		return
 	}
-	if child, ok := r.children[stack[0]]; ok {
+	if child, ok := r.Children[stack[0]]; ok {
 		child.Add(stack[1:], value)
 	} else {
 		child = NewRecord(stack[0], 0)
@@ -33,13 +33,13 @@ func (r *Record) Add(stack []string, value int) {
 
 // AddChild adds a child record
 func (r *Record) AddChild(child *Record) {
-	r.children[child.name] = child
+	r.Children[child.Name] = child
 }
 
 // ReduceRoot returns the only child if len(children) == 1, otherwise the root itself
 func (r *Record) ReduceRoot() *Record {
-	if len(r.children) == 1 {
-		for _, child := range r.children {
+	if len(r.Children) == 1 {
+		for _, child := range r.Children {
 			return child
 		}
 	}
@@ -48,9 +48,9 @@ func (r *Record) ReduceRoot() *Record {
 
 // FixRootValue fix value using sum of children values
 func (r *Record) FixRootValue() *Record {
-	r.value = 0
-	for _, child := range r.children {
-		r.value += child.value
+	r.Value = 0
+	for _, child := range r.Children {
+		r.Value += child.Value
 	}
 	return r
 }
--- flamewriter/writer_json.go
+++ flamewriter/writer_json.go
@@ -18,13 +18,13 @@ func NewJSONWriter(w io.Writer) *JSONWriter {
 }
 
 func toJSONObject(root *Record) map[string]interface{} {
-	children := make([]map[string]interface{}, 0, len(root.children))
-	for _, child := range root.children {
+	children := make([]map[string]interface{}, 0, len(root.Children))
+	for _, child := range root.Children {
 		children = append(children, toJSONObject(child))
 	}
 	return map[string]interface{}{
-		"name":     root.name,
-		"value":    root.value,
+		"name":     root.Name,
+		"value":    root.Value,
 		"children": children,
 	}
 }
