fac476a8f08731348286d821d508a8cccf6d5d3f
杨文轩 <gzyangwenxuan@corp.netease.com>
2019-06-11 14:03:09 +0800

    revert make members public

--- flamewriter/writer_json.go
+++ flamewriter/writer_json.go
@@ -18,13 +18,13 @@ func NewJSONWriter(w io.Writer) *JSONWriter {
 }
 
 func toJSONObject(root *Record) map[string]interface{} {
-	children := make([]map[string]interface{}, 0, len(root.Children))
-	for _, child := range root.Children {
+	children := make([]map[string]interface{}, 0, len(root.children))
+	for _, child := range root.children {
 		children = append(children, toJSONObject(child))
 	}
 	return map[string]interface{}{
-		"name":     root.Name,
-		"value":    root.Value,
+		"name":     root.name,
+		"value":    root.value,
 		"children": children,
 	}
 }
